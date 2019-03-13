baa259159e08340cd8663f73375c83ebe5cfa4ea
liuzhao <liuzhao612525@163.com>
2019-03-13 19:20:56 +0800

    first commit

---
+++ src/cn/itcast/action/BookAction.java
@@ -0,0 +1,15 @@
+package cn.itcast.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BookAction extends ActionSupport {
+	@Override
+	public String execute() throws Exception {
+		
+		return SUCCESS;
+	}
+	
+	public String  price() {
+		return "100";
+	}
+}
---
+++ src/cn/itcast/action/OrderAction.java
@@ -0,0 +1,11 @@
+package cn.itcast.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class OrderAction extends ActionSupport {
+	@Override
+	public String execute() throws Exception {
+		
+		return SUCCESS;
+	}
+}
